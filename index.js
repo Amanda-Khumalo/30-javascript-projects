eaeae1241f3f17351354c32036945a9d85d048da
amanda-khumalo <amandapkhumalo@icloud.com>
2025-03-21 17:32:51 +0200

    heart

---
+++ index.js
@@ -0,0 +1,15 @@
+const bodyEl = document.querySelector("body");
+
+bodyEl.addEventListener("mousemove", (event) =>{
+    const xPos = event.offsetX; 
+    const xPos = event.offsetX;
+    const spanEl = document.createdElement("span"); 
+    spanEl.style.left = xPos + "px"; 
+    spanEl.style.top = yPos + "px";
+    const size = Math.random() * 100; 
+    
+
+
+
+
+})
